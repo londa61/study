a8ccad780af09677183f9dc5d3fdf0f4e6b43c95
lingzi <fenglingzi1995@gmail.com>
2023-03-06 18:35:03 +0800

    hello

---
+++ DesignPatterns/code/src/main/java/HelloTest.java
@@ -0,0 +1,13 @@
+/**
+ * zlz
+ *
+ * @author flz
+ * @version 1.0
+ * @description: init test
+ * @date 17:30 2023/3/6
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
