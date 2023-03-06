41a8c9d3a255d283f5d1e648d7f2e37b0254c3bd
lingzi <fenglingzi1995@gmail.com>
2023-03-06 17:14:58 +0800

    design-patterns初始化

---
+++ DesignPatterns/code/src/main/java/TestHello.java
@@ -0,0 +1,13 @@
+/**
+ * zlz
+ *
+ * @author flz
+ * @version 1.0
+ * @description:
+ * @date 17:11 2023/3/6
+ */
+public class TestHello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
